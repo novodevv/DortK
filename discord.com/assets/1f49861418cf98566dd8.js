4922b7365695c68c3743ca45af2f91c59141fada
kay <116453182+novodevv@users.noreply.github.com>
2022-11-22 00:18:53 -0500

    Delete discord.com directory

--- discord.com/assets/1f49861418cf98566dd8.js
+++
@@ -1,9 +0,0 @@
-(this.webpackChunkdiscord_app = this.webpackChunkdiscord_app || []).push([
-  [91596],
-  {
-    491596: (p, s, a) => {
-      p.exports = a.p + "6a4f76f9849935bd57822e9428481f1c.woff2";
-    },
-  },
-]);
-//# sourceMappingURL=1f49861418cf98566dd8.js.map
